cb45b1e626c7f2d3ce5ed6984c5fd2ef2e723208
Farrel Abyansyah <farrel.abyansyah@gmail.com>
2022-11-17 08:00:33 +0700

    add config files

---
+++ config/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "dz4loq8cg",
+  api_key: "618292679384394",
+  api_secret: "rYERSpTeKmD0bSz7Z5ktpU9Ov6s",
+});
+
+module.exports = cloudinary;
