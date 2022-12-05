d7f7a9daa71c392f22fb63f523378818ad793583
Farrel Abyansyah <farrel.abyansyah@gmail.com>
2022-12-05 19:59:47 +0700

    initial swagger commit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const PORT = 8050 || process.env.PORT;
-
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 const app = express();
 
 const router = require("./router/router");
@@ -9,3 +10,33 @@ app.use(router);
 app.listen(PORT, () => {
   console.log(`Application running in http://localhost:${PORT}`);
 });
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Challenge 06 API Documentation",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Binar Synrgy",
+        url: "https://binaracademy.com",
+        email: "binaracademy.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:8050/posts",
+      },
+    ],
+  },
+  apis: ["./router/router.js"],
+};
+const specs = swaggerJsDoc(options);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
--- router/router.js
+++ router/router.js
@@ -28,12 +28,134 @@ const auth = require("../middleware/auth");
 //   secret:'akusuper'
 // }
 
+/**
+ * @swagger
+ *  components:
+ *   schemas:
+ *     user_login:
+ *      type: object
+ *      required:
+ *        - email
+ *        - password
+ *      properties:
+ *        id:
+ *          type: integer
+ *          description: The generated ID's user_login model
+ *        email:
+ *          type: string
+ *          description: The user's email
+ *        password:
+ *          type: string
+ *          description: The user's hashed password
+ *        role:
+ *          type: string
+ *          description: The user's role. Used for authorization
+ *        createdAt:
+ *          type: timestamp
+ *          description: The Created data date
+ *        updatedAt:
+ *          type: timestamp
+ *          description: The updated data date
+ */
+
+/**
+ * @swagger
+ *  components:
+ *   schemas:
+ *     cars:
+ *      type: object
+ *      required:
+ *        - brand
+ *        - model
+ *        - year
+ *      properties:
+ *        id:
+ *          type: integer
+ *          description: The generated ID's Post model
+ *        brand:
+ *          type: string
+ *          description: The car's brand
+ *        model:
+ *          type: string
+ *          description: The car's model
+ *        year:
+ *          type: integer
+ *          description: The car's manufactured year
+ *        createdby:
+ *          type: string
+ *          description: The user who created the data
+ *        updatedby:
+ *          type: string
+ *          description: The user who updated the data
+ *        deletedby:
+ *          type: string
+ *          description: The user who deleted the data
+ *        createdAt:
+ *          type: timestamp
+ *          description: The Created data date
+ *        updatedAt:
+ *          type: timestamp
+ *          description: The updated data date
+ *        deletedAt:
+ *          type: timestamp
+ *          description: The soft data date
+ */
+
+/**
+ * @swagger
+ *   /getadmins/:
+ *      get:
+ *        summary: Get all users
+ *        tags: [user_login]
+ *        responses:
+ *          "200":
+ *            description: Retrieve all users data
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  $ref: '#/components/schemas/Post'
+ * @swagger
+ *   /super/:
+ *      post:
+ *        summary: Posts superadmin with secret key
+ *        tags: [user_login]
+ *        requestBody:
+ *          required:true
+ *        responses:
+ *          "200":
+ *            description: Retrieve posts by id
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  $ref: '#/components/schemas/Post'
+ */
+
 router.post("/super", encryptPassword, registerSuper);
-router.get("/", getAdmin);
+router.get("/getadmins", getAdmin);
 router.post("/admin", auth, encryptPassword, registerAdmin);
 router.post("/user", auth, encryptPassword, registerUser);
 router.post("/login", loginCheck);
 router.get("/token", auth, tokenCheck);
+
+/**
+ * @swagger
+ *   /posts/{id}:
+ *      get:
+ *        summary: Get posts by id
+ *        tags: [Posts]
+ *        parameters:
+ *          - in: path
+ *            name: id
+ *          - in:path
+ *            kontil:id
+ *        responses:
+ *          "200":
+ *            description: Retrieve posts by id
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  $ref: '#/components/schemas/Post'
+ */
 
 router.post("/cars/add", auth, addCar);
 router.put("/cars/update/:id", auth, updateCar);
